838ef6ae2019fd46c602c7887b4f73415d936c97
Nan Xiao <nan@chinadtrace.org>
2016-08-31 03:07:52 -0400

    Upload source code.

---
+++ client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/NanXiao/client/api"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "/tmp/node-1/swarm.sock"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewControlClient(conn)
+
+	r, err := c.ListNodes(context.Background(), &pb.ListNodesRequest{})
+	if err != nil {
+		log.Fatalf("Could not list nodes: %v", err)
+	}
+	log.Printf("List nodes: %+v", r)
+}
